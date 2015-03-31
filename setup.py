78c218744f90c60f6ee76de05ecc8313f42c8fe2
Adam Drakeford <adam.drakeford@dedsert.com>
2015-03-31 18:38:36 +0100

    updated setup.py and license

--- setup.py
+++ setup.py
@@ -10,28 +10,31 @@ from setuptools import setup, find_packages
 
 setup(
     name='txGraylog',
-    version='1.0',
+    version='0.3',
     description='A twisted log service that interacts with Graylog2.',
+    long_description=(
+        'txGraylog is a a twisted based client that interacts with a '
+        'Graylog server. Currently it supports the following protocols '
+        'UDP and TCP protocols.'),
     author='Adam Drakeford',
     author_email='adamdrakeford@gmail.com',
     license='MIT',
+    url='https://github.com/dr4ke616/txGraylog/',
     packages=find_packages(),
     tests_require=['twisted>=10.2.0', 'coverage', 'jsonlib'],
     install_requires=['twisted>=10.2.0', 'jsonlib'],
     requires=['twisted(>=10.2.0)'],
     zip_safe=False,
     classifiers=[
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Development Status :: 3 - Alpha',
-        'Framework :: Twisted',
-        'Intended Audience :: Developers',
-        'License :: MIT License (MIT)',
-        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Logging',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
